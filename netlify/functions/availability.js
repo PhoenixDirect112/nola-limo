7af1ce3401028ed9440ac3cbc3eb16c61bdb568d
PhoenixDirect112 <AvalinWilliams@gmail.com>
2026-04-27 20:07:20 -0500

    Calender Updates

--- netlify/functions/availability.js
+++ netlify/functions/availability.js
@@ -95,7 +95,7 @@ function parseCentralDateTime(dtStr) {
 }
 
 function eventsToBookedSlots(events) {
-  const ALL_TIMES = ['06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00'];
+  const ALL_TIMES = ['06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00','00:00'];
   const slots = {};
   for (const event of events) {
     const start = parseCentralDateTime(event.start.dateTime);
